e2b0d8148b854b494955605a02cd9b43cd1dd159
Sean Wu <sean_wu@edge-core.com>
2021-07-26 01:50:13 +0000

    Take 1st arg (optional) as repo src root
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- sonic/sonic_accton_pbuilder.sh
+++ sonic/sonic_accton_pbuilder.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-PATH_TO_SONIC_BUILDIMAGE_DIR=/home/ubuntu/src/sonic-buildimage
+if [ -d "$1" ]
+then
+	PATH_TO_SONIC_BUILDIMAGE_DIR=`realpath $1`
+else
+	PATH_TO_SONIC_BUILDIMAGE_DIR=/home/ubuntu/src/sonic-buildimage
+fi
+
 #SLAVEIMAGE=sonic202012-barefoot-pbuilder:latest
 SLAVEIMAGE=sonic-accton-pbuilder:ef67ba5f6
 ENV_LINUX_HEADER="KERNEL_SRC=/usr/src/linux-headers-4.19.0-12-2-amd64/"
